fc8558ba2ddc2f5e672645631f89c05fc4024b67
addledo <jontyfpine@gmail.com>
2026-02-12 20:00:27 +0000

    Add delphi formatter to conform

--- lua/jonty/plugins/conform.lua
+++ lua/jonty/plugins/conform.lua
@@ -20,12 +20,25 @@ return {
 
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      pascal = { 'delphi_formatter' },
     },
-  },
+
+    formatters = {
+      delphi_formatter = {
+        inherit = false,
+        stdin = false,
+        command = 'Formatter.exe',
+        args = function()
+          local repo = vim.fs.root(0, '.git')
+          return {
+            '-config',
+            repo .. '/DevelopmentEnvironment/delphi_formatter.config',
+            '$FILENAME'
+          }
+          end,
+      }, --delphi
+
+    }, -- formatters
+  }, -- opts
 }
 -- vim: ts=2 sts=2 sw=2 et
