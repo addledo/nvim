1d495be6fc91e54ab5808948d5659369aabd2931
addledo <jontyfpine@gmail.com>
2026-02-13 15:17:46 +0000

    Add keymaps

--- lua/jonty/map.lua
+++ lua/jonty/map.lua
@@ -1,3 +1,23 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Window switching
+vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Close buffer
+vim.keymap.set('n', '<leader>x', '<CMD>:bd<CR>', { desc = 'Close Buffer' })
+
+-- Select all
+vim.keymap.set('n', '<leader>fh', 'ggVG', { desc = 'Select all' })
+
+-- Yank all
+vim.keymap.set('n', '<leader>fy', function()
+  local cur = vim.fn.getpos('.')
+  vim.cmd('keepjumps normal! gg"+yG')
+  vim.fn.setpos('.', cur)
+end, { desc = 'Yank whole file to clipboard' })
+
+-- Keymaps for sourcing lua
+vim.keymap.set('n', '<leader>x', ':.lua<CR>', { desc = 'Source current line' })
+vim.keymap.set('v', '<leader>x', ':lua<CR>', { desc = 'Source current selection' })
