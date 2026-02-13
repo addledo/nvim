23975e440e5c2fd0d012e3d8f75a95956b2f259c
addledo <jontyfpine@gmail.com>
2026-02-13 18:14:13 +0000

    Add map for esc to nohl

--- lua/jonty/map.lua
+++ lua/jonty/map.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- Window switching
 vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
