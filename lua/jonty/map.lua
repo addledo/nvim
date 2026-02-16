0ce9e1635cc5603406e0cfa1a53c2c63e2f76f48
Jonty Pine <jonty@Jontys-MacBook-Air.local>
2026-02-16 18:35:15 +0000

    Improve navigation with Yazi and keymaps

--- lua/jonty/map.lua
+++ lua/jonty/map.lua
@@ -24,3 +24,19 @@ end, { desc = 'Yank whole file to clipboard' })
 -- Keymaps for sourcing lua
 vim.keymap.set('n', '<leader>x', ':.lua<CR>', { desc = 'Source current line' })
 vim.keymap.set('v', '<leader>x', ':lua<CR>', { desc = 'Source current selection' })
+
+-- cd to repo root
+vim.keymap.set("n", "<leader>dr", function()
+  local root = vim.fs.root(0, { ".git" })
+  if root then vim.cmd.cd(root) end
+end)
+
+-- cd to current file's directory
+vim.keymap.set("n", "<leader>d.", function()
+  vim.cmd.cd(vim.fn.expand("%:p:h"))
+end)
+
+-- cd one up
+vim.keymap.set("n", "<leader>du", function()
+  vim.cmd.cd("..")
+end)
