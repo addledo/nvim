855f715140bcf586dab0002fced7b2c7d024798e
addledo <jontyfpine@gmail.com>
2026-02-16 22:33:18 +0000

    Add toggleterm

---
+++ lua/jonty/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  'akinsho/toggleterm.nvim',
+  enabled = true,
+  version = '*',
+  opts = {
+    open_mapping = [[<c-\>]],
+    direction = 'float',
+    float_opts = { border = 'curved' },
+  },
+}
