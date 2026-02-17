6a98c88a07c25ce993e9b9396a77a346806bc881
addledo <jontyfpine@gmail.com>
2026-02-17 14:17:10 +0000

    Update diffview

--- lua/jonty/plugins/diffview.lua
+++ lua/jonty/plugins/diffview.lua
@@ -1,7 +1,12 @@
 return {
   'sindrets/diffview.nvim',
 
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+
   opts = {
-    vim.keymap.set('n', '<leader>dm', '<cmd>DiffviewOpen main...HEAD<CR>', { desc = 'Diff main...HEAD' })
+    vim.keymap.set('n', '<leader>dm', '<cmd>DiffviewOpen main...HEAD<CR>', { desc = 'Diff main...HEAD' }),
+    vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = 'DiffviewClose' }),
   }
 }
