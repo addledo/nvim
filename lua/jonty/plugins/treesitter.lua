d39e977b628e5f86d8f388af1a9e8194f96be44c
addledo <jontyfpine@gmail.com>
2026-02-17 15:55:38 +0000

    Set treesitter to user master branch

--- lua/jonty/plugins/treesitter.lua
+++ lua/jonty/plugins/treesitter.lua
@@ -1,11 +1,27 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    main = 'nvim-treesitter.configs',
     lazy = false,
     build = ':TSUpdate',
 
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'pascal',
+        'python',
+     },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
