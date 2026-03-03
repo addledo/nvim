9e55ed7161b01453d75b31edaa0e82428cae9c83
addledo <jontyfpine@gmail.com>
2026-03-03 18:08:59 +0000

    Tidy comments in blink.lua

--- lua/jonty/plugins/blink.lua
+++ lua/jonty/plugins/blink.lua
@@ -3,7 +3,6 @@ return {
 
   enabled = true,
 
-  -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
 
   version = '1.*',
@@ -23,8 +22,6 @@ return {
     keymap = { preset = 'super-tab' },
 
     appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
     },
 
@@ -38,11 +35,6 @@ return {
       default = { 'path', 'snippets', 'buffer' },
     },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
     fuzzy = { implementation = "prefer_rust_with_warning" }
   },
   opts_extend = { "sources.default" }
