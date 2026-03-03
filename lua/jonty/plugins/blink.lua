fe806915807aec11a07f466e05440c524fc215db
addledo <jontyfpine@gmail.com>
2026-03-03 20:33:53 +0000

    Try add lazydev and lua lsp

--- lua/jonty/plugins/blink.lua
+++ lua/jonty/plugins/blink.lua
@@ -28,9 +28,15 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       -- default = { 'lsp', 'path', 'snippets', 'buffer' },
-      default = { 'ripgrep', 'path', 'snippets', 'buffer' },
+      default = { 'lazydev', 'ripgrep', 'path', 'snippets', 'buffer' },
 
       providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
         ripgrep = {
           module = 'blink-ripgrep',
           name = 'Ripgrep',
