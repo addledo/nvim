64d484a848b130c8a53aa394cf86f0943d0b74ac
addledo <jontyfpine@gmail.com>
2026-03-04 10:14:24 +0000

    Re-enable DelphiLSP

--- lua/jonty/init.lua
+++ lua/jonty/init.lua
@@ -9,18 +9,18 @@ end
 
 require 'jonty.lazy'
 
--- vim.lsp.enable('delphi_lsp')
+vim.lsp.enable 'delphi_lsp'
 
 -- Filter annoying warnings for DelphiLSP
--- vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
---   local client = vim.lsp.get_client_by_id(ctx.client_id)
---   local client_name = client and client.name or ''
---
---   -- Suppress the DelphiLSP respawn message
---   if client_name == 'delphi_lsp' and result.message:match 'terminated and will be respawned' then
---     return
---   end
---
---   -- Show all other messages normally
---   vim.notify(result.message, vim.log.levels.INFO)
+vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
+  local client_name = client and client.name or ''
+
+  -- Suppress the DelphiLSP respawn message
+  if client_name == 'delphi_lsp' and result.message:match 'terminated and will be respawned' then
+    return
+  end
+
+  -- Show all other messages normally
+  vim.notify(result.message, vim.log.levels.INFO)
 end
