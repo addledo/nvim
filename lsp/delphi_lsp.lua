9cbd3e7a095b2b03fb16064c468bfde63208d581
addledo <jontyfpine@gmail.com>
2026-03-09 19:58:02 +0000

    Update delphi lsp root

--- lsp/delphi_lsp.lua
+++ lsp/delphi_lsp.lua
@@ -1,5 +1,31 @@
 return {
-    cmd = { 'DelphiLSP64.exe' },
-    filetypes = { 'pascal' },
-    root_markers = { '.git' },
+  cmd = { 'Delphi13LSP64.exe' },
+  filetypes = { 'pascal' },
+  -- root_markers = { 'MotorCAD_Group.groupproj', '.git' },
+
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local start = vim.fs.dirname(fname)
+
+    local groupproj = vim.fs.find(function(name)
+      return name:match '%.groupproj$' ~= nil
+    end, {
+      path = start,
+      upward = true,
+      stop = vim.uv.os_homedir(),
+      type = 'file',
+      limit = 1,
+    })[1]
+
+    local git = vim.fs.find('.git', {
+      path = start,
+      upward = true,
+      stop = vim.uv.os_homedir(),
+      limit = 1,
+    })[1]
+
+    local marker = groupproj or git
+    -- Return dir of marker, if dir exists. Otherwise return cwd.
+    on_dir(marker and vim.fs.dirname(marker) or vim.fn.getcwd())
+  end,
 }
