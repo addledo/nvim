120737ab69c86215f2e05d438ac8b7033a3288c7
addledo <jontyfpine@gmail.com>
2026-03-18 10:59:56 +0000

    Add git blame keymap

--- lua/jonty/plugins/git-fugitive.lua
+++ lua/jonty/plugins/git-fugitive.lua
@@ -1,8 +1,22 @@
 return {
-    'tpope/vim-fugitive',
-    cmd = {
-      "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit",
-      "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
-      "Gdelete", "Gremove", "Gbrowse",
-    },
+  'tpope/vim-fugitive',
+  cmd = {
+    'G',
+    'Git',
+    'Gdiffsplit',
+    'Gvdiffsplit',
+    'Gedit',
+    'Gsplit',
+    'Gread',
+    'Gwrite',
+    'Ggrep',
+    'Glgrep',
+    'Gmove',
+    'Gdelete',
+    'Gremove',
+    'Gbrowse',
+  },
+  keys = {
+    { '<leader>gB', '<cmd>G blame<cr>', desc = 'Git blame' },
+  },
 }
