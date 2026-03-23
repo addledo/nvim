8319e70f0eb5d4fcb5f4e24563610021c97418a5
addledo <jontyfpine@gmail.com>
2026-03-23 18:14:06 +0000

    Update themes

--- lua/jonty/plugins/theme.lua
+++ lua/jonty/plugins/theme.lua
@@ -1,21 +1,56 @@
 return {
-  'EdenEast/nightfox.nvim',
 
-  lazy = false,
-  priority = 1000,
+  -- Sonokai
+  {
+    'sainnhe/sonokai',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'sonokai'
+    end,
+  },
 
-  config = function ()
-    require('nightfox').setup {
-      options = {
-        colorblind = {
-          enable = true,
-          severity = {
-            deutan = 1,
+  -- Github
+  {
+    'projekt0n/github-nvim-theme',
+    enabled = false,
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {}
+      -- vim.cmd 'colorscheme github_dark_colorblind'
+    end,
+  },
+
+  -- Monokai
+  {
+    'loctvl842/monokai-pro.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('monokai-pro').setup()
+      -- vim.cmd.colorscheme 'monokai-pro'
+    end,
+  },
+
+  -- Nightfox
+  {
+    'EdenEast/nightfox.nvim',
+    enabled = false,
+    config = function()
+      require('nightfox').setup {
+        options = {
+          colorblind = {
+            enable = true,
+            severity = {
+              deutan = 1,
+            },
           },
         },
-      },
-    vim.cmd.colorscheme 'terafox',
-    }
-  end,
+        -- vim.cmd.colorscheme 'terafox',
+      }
+    end,
+  },
 }
-
