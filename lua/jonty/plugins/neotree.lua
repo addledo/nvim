8f3d92d5414a4c827496bc0270e80fa9397ca236
addledo <jontyfpine@gmail.com>
2026-04-07 12:31:30 +0100

    Allow neotree to self-lazy-load

--- lua/jonty/plugins/neotree.lua
+++ lua/jonty/plugins/neotree.lua
@@ -6,7 +6,7 @@ return {
     'MunifTanjim/nui.nvim',
     'nvim-tree/nvim-web-devicons', -- optional, but recommended
   },
-  -- lazy = false, -- neo-tree will lazily load itself
+  lazy = false, -- neo-tree will lazily load itself
   cmd = 'Neotree',
   config = function()
     vim.keymap.set('n', '<leader>b', '<cmd>Neotree source=buffers position=right<CR>')
