be71ed696b3c72e1d9b02e9954fbf36597c2ad07
addledo <jontyfpine@gmail.com>
2026-04-11 09:56:34 +0100

    Add qmk

---
+++ lua/jonty/plugins/qmk.lua
@@ -0,0 +1,14 @@
+return {
+  'codethread/qmk.nvim',
+  event = 'VeryLazy',
+  opts = {
+    name = 'LAYOUT_34',
+    variant = 'zmk',
+    layout = {
+      'x x x x x _ x x x x x',
+      'x x x x x _ x x x x x',
+      'x x x x x _ x x x x x',
+      '_ _ _ x x _ x x _ _ _',
+    },
+  },
+}
