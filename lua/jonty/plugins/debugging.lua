206d9f6542a83b2f543970ec89c2b1f9e7e308c9
addledo <jontyfpine@gmail.com>
2026-04-16 20:56:01 +0100

    Add dap python - currently disabled

--- lua/jonty/plugins/debugging.lua
+++ lua/jonty/plugins/debugging.lua
@@ -26,11 +26,14 @@ return {
       local ui = require 'dapui'
 
       ui.setup()
-      require('nvim-dap-virtual-text').setup()
+      require('nvim-dap-virtual-text').setup({})
 
       require('mason-nvim-dap').setup {
         automatic_installation = true,
-        ensure_installed = { 'codelldb' },
+        ensure_installed = {
+          'codelldb',
+          -- 'python',
+        },
         handlers = {},
       }
 
@@ -47,20 +50,16 @@ return {
         },
       }
 
-      -- vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
-      -- vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
-      --
-      -- -- Eval var under cursor
-      -- vim.keymap.set('n', '<space>?', function()
-      --   ui.eval(nil, { enter = true })
-      -- end)
-      --
-      -- vim.keymap.set('n', '<F1>', dap.continue, { desc = 'Debugger continue' })
-      -- vim.keymap.set('n', '<F2>', dap.step_into, { desc = 'Debugger step into' })
-      -- vim.keymap.set('n', '<F3>', dap.step_over, { desc = 'Debugger step over' })
-      -- vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'Debugger step out' })
-      -- vim.keymap.set('n', '<F5>', dap.step_back, { desc = 'Debugger step back' })
-      -- vim.keymap.set('n', '<F13>', dap.restart, { desc = 'Debugger restart' })
+
+      -- dap.configurations.python = {
+      --   {
+      --     type = 'python',
+      --     request = 'launch',
+      --     name = 'Launch file',
+      --     program = '${file}',
+      --   },
+      -- }
+
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
