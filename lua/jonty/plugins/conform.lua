0596983609a1679f5257f372c9d5ce3a09072180
addledo <jontyfpine@gmail.com>
2026-04-16 20:50:55 +0100

    Small fix for conform

--- lua/jonty/plugins/conform.lua
+++ lua/jonty/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
   config = function()
     require('conform').setup {
       notify_on_error = true,
-      format_on_save = false,
+      format_on_save = nil,
 
       formatters_by_ft = {
         lua = { 'stylua' },
