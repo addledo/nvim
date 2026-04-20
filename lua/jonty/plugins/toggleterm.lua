8822a707c5ea13dc10fd3bfad11a95c6a7893cc7
addledo <jontyfpine@gmail.com>
2026-04-20 17:31:09 +0100

    Conditional shell in toggleterm

--- lua/jonty/plugins/toggleterm.lua
+++ lua/jonty/plugins/toggleterm.lua
@@ -3,9 +3,22 @@ return {
   event = 'VeryLazy',
   enabled = true,
   version = '*',
-  opts = {
-    open_mapping = [[<c-\>]],
-    direction = 'float',
-    float_opts = { border = 'curved' },
-  },
+  config = function()
+    local sh
+    if jit then
+      if jit.os == "Windows" then
+        sh = vim.fn.executable("pwsh") and "pwsh" or "powershell"
+      elseif vim.fn.executable("zsh") then
+        vim.o.shell = "zsh"
+      else
+        vim.o.shell = "bash"
+      end
+    end
+    require 'toggleterm'.setup {
+      shell = sh,
+      open_mapping = [[<c-\>]],
+      direction = 'float',
+      float_opts = { border = 'curved' },
+    }
+  end
 }
