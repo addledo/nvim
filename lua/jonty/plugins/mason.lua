29eac8467d985c219f569613f33fa3a1bb631dd5
addledo <jontyfpine@gmail.com>
2026-04-28 11:04:11 +0100

    Remove bad tree-sitter install from mason config

--- lua/jonty/plugins/mason.lua
+++ lua/jonty/plugins/mason.lua
@@ -2,7 +2,7 @@ return {
   'mason-org/mason-lspconfig.nvim',
   event = 'VeryLazy',
   opts = {
-    ensure_installed = { 'lua_ls', 'tree-sitter-cli' },
+    ensure_installed = { 'lua_ls' },
   },
   dependencies = {
     { 'mason-org/mason.nvim', opts = {} },
