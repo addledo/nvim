96cc053655b22cdd51d577aeaf72db763fa1d690
addledo <jontyfpine@gmail.com>
2026-05-18 15:25:31 +0100

    Add auto-session

--- lua/jonty/dashboard.lua
+++ lua/jonty/dashboard.lua
@@ -19,7 +19,7 @@ return {
       { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
       { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
       { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      { icon = " ", key = "s", desc = "Restore Session", action = ":Autosession restore" },
       { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
       { icon = "Y",  key = "y", desc = "Yazi", action = ":Yazi", enabled = package.loaded.lazy ~= nil },
       { icon = " ", key = "q", desc = "Quit", action = ":qa" },
---
+++ lua/jonty/plugins/auto-session.lua
@@ -0,0 +1,20 @@
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+
+  keys = {
+    -- Will use Telescope if installed or a vim.ui.select picker otherwise
+    { "<leader>wr", "<cmd>AutoSession search<CR>", desc = "Session search" },
+    { "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save session" },
+    { "<leader>wa", "<cmd>AutoSession toggle<CR>", desc = "Toggle autosave" },
+  },
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    -- suppressed_dirs = { "~/", "~/Downloads", "/" },
+    -- log_level = 'debug',
+    auto_restore = false,
+  },
+}
