f7e9b87fce7d7cce3dd3eb8828b7618c3e14121a
addledo <jontyfpine@gmail.com>
2026-05-22 17:01:31 +0100

    Add overseer

---
+++ lua/jonty/plugins/overseer.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/overseer.nvim',
+  ---@module 'overseer'
+  ---@type overseer.SetupOpts
+  opts = {},
+  keys = {
+    { "<leader>o", "<cmd>OverseerToggle<CR>", desc = "Overseer Toggle" },
+  },
+}
