2b1deca3aecaa763a5e47347a79191fb1502a9bb
addledo <jontyfpine@gmail.com>
2026-06-03 10:50:24 +0100

    Add hex keymaps

--- lua/jonty/plugins/hex.lua
+++ lua/jonty/plugins/hex.lua
@@ -2,4 +2,9 @@ return
 {
     'RaafatTurki/hex.nvim',
     opts = {},
+    keys = {
+        -- { "<leader>Hd", "<cmd>HexDump<CR>", desc = "HexDump" },
+        -- { "<leader>Ha", "<cmd>HexAssemble<CR>", desc = "HexAssemble" },
+        { "<leader>ht", "<cmd>HexToggle<CR>", desc = "HexToggle" },
+    },
 }
