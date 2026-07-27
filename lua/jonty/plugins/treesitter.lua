71bb9c75a54687094584cca639be98cbbf857433
addledo <jontyfpine@gmail.com>
2026-07-27 09:00:25 +0100

    Create folds on start

--- lua/jonty/plugins/treesitter.lua
+++ lua/jonty/plugins/treesitter.lua
@@ -42,8 +42,10 @@ return {
         end
 
         -- replicate `fold = { enable = true }`
-        -- vim.wo.foldmethod = 'expr'
-        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo.foldmethod = 'expr'
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.opt.foldlevelstart = 99
+        vim.opt.foldlevel = 99
 
         -- replicate `highlight = { enable = true }`
         vim.treesitter.start(buf, language)
